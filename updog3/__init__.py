ff9c2aa8e851c47d4e54ddc8f8de02c1f2f121a3
Felipe Molina <846513+felmoltor@users.noreply.github.com>
2024-11-05 12:05:54 +0000

    fix __init__ to use full path

--- updog3/__init__.py
+++ updog3/__init__.py
@@ -1,4 +1,8 @@
-# Version is coming from the file version
-version = 'v'+open('version','r').read().strip()
+import os
+
+# Get the absolute path to the version file
+root_dir = os.path.dirname(os.path.dirname(__file__))
+with open(os.path.join(root_dir, 'version'), 'r') as f:
+    version = 'v' + f.read().strip()
 
 base_directory = ''
